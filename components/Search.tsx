3323cd1979adda70ff47ebc10e6710121de1e408
bamboo <minnie20000622@yahoo.co.jp>
2022-08-10 00:07:27 +0900

    update Search.tsx

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,25 +1,43 @@
 import React from "react";
+
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { InputAdornment } from "@material-ui/core";
+
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 
+const suggestPlaces = [{ label: "現在地" }, { label: "サポーターズ本社" }];
+
 export function Search() {
   return (
-    <Paper
-      component="form"
-      sx={{ p: "2px 4px", display: "flex", alignItems: "center" }}
-    >
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="渋谷駅"
-        inputProps={{ "aria-label": "search google maps" }}
-      />
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-    </Paper>
+    <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      options={suggestPlaces}
+      sx={{ width: 300 }}
+      freeSolo
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="どこでデートする？"
+          placeholder="東京駅"
+          sx={{ width: "100%" }}
+          InputProps={{
+            ref: params.InputProps.ref,
+            endAdornment: (
+              <>
+                <InputAdornment position="end">
+                  <SearchIcon />
+                </InputAdornment>
+              </>
+            ),
+          }}
+        />
+      )}
+    />
   );
 }
