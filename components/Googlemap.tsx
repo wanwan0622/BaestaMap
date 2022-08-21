2acf1a44d5ae1c3376e6d1a509d2abc1d6da1f6e
bamboo <minnie20000622@yahoo.co.jp>
2022-08-21 21:04:40 +0900

    GoogleMapの余計なボタンを非表示

--- components/Googlemap.tsx
+++ components/Googlemap.tsx
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import React, { Component, useState } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
 const containerStyle = {
   width: "300px",
   height: "300px",
+};
+
+const options = {
+  disableDefaultUI: true,
+  zoomControl: true,
 };
 
 const center = {
@@ -11,12 +22,32 @@ const center = {
   lng: 136, // 東経
 };
 
+const positionMarker = {
+  lat: 36,
+  lng: 136,
+};
+
 export function Googlemap() {
+  const [size, setSize] = useState<undefined | google.maps.Size>(undefined);
+  const infoWindowOptions = {
+    pixelOffset: size,
+  };
+
   return (
-    <LoadScript googleMapsApiKey={process.env.GOOGLE_MAP_API_KEY ?? ""}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+    <LoadScript googleMapsApiKey={"AIzaSyAnDeddlbLcZsaRzZ2ZmtP_ONVtmVrnKZM"}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        options={options}
+      >
         {/* Child components, such as markers, info windows, etc. */}
-        <></>
+        <Marker position={positionMarker} />
+        <InfoWindow position={positionMarker} options={infoWindowOptions}>
+          <div style={{}}>
+            <h1>秋葉原オフィス</h1>
+          </div>
+        </InfoWindow>
       </GoogleMap>
     </LoadScript>
   );
